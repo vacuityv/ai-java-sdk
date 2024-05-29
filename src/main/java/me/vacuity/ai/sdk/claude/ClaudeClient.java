9fa40b413ec204b0afebc3375ecb1a7945eedb9a
HuanWang <894584459@qq.com>
2024-05-30 00:36:14 +0800

    fix claude name and optimize ObjectMapper

--- src/main/java/me/vacuity/ai/sdk/claude/ClaudeClient.java
+++ src/main/java/me/vacuity/ai/sdk/claude/ClaudeClient.java
@@ -3,7 +3,7 @@ package me.vacuity.ai.sdk.claude;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.Single;
@@ -135,7 +135,7 @@ public class ClaudeClient {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
         mapper.addMixIn(ChatFunction.class, ChatFunctionMixIn.class);
         return mapper;
     }
