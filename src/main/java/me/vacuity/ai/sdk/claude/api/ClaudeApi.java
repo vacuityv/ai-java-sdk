66e9df1b7f9fcda48cedeee77879ced41b5b0ed6
vacuity <vacuityv@icloud.com>
2024-11-04 13:16:49 +0800

    ✨feature(1.7.10): claude
    - add claude beta header:anthropic-beta: pdfs-2024-09-25 to support pdf

--- src/main/java/me/vacuity/ai/sdk/claude/api/ClaudeApi.java
+++ src/main/java/me/vacuity/ai/sdk/claude/api/ClaudeApi.java
@@ -12,12 +12,12 @@ import retrofit2.http.Streaming;
 
 public interface ClaudeApi {
 
-    @Headers({"anthropic-version: 2023-06-01"})
+    @Headers({"anthropic-version: 2023-06-01", "anthropic-beta: pdfs-2024-09-25"})
     @POST("v1/messages")
     Single<ChatResponse> chat(@Body ChatRequest request);
 
 
-    @Headers({"anthropic-version: 2023-06-01"})
+    @Headers({"anthropic-version: 2023-06-01", "anthropic-beta: pdfs-2024-09-25"})
     @Streaming
     @POST("v1/messages")
     Call<ResponseBody> streamChat(@Body ChatRequest request);
