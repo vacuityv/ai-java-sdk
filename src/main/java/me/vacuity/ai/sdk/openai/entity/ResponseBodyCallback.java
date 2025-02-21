88126d7b57152a71b3e4c0e6126efcf1cae6df41
vacuity <vacuityv@icloud.com>
2025-02-21 18:31:59 +0800

    ✨feature(1.7.24): bug fixed

--- src/main/java/me/vacuity/ai/sdk/openai/entity/ResponseBodyCallback.java
+++ src/main/java/me/vacuity/ai/sdk/openai/entity/ResponseBodyCallback.java
@@ -2,9 +2,9 @@ package me.vacuity.ai.sdk.openai.entity;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.reactivex.FlowableEmitter;
-import me.vacuity.ai.sdk.claude.error.ChatResponseError;
-import me.vacuity.ai.sdk.claude.exception.VacSdkException;
 import me.vacuity.ai.sdk.openai.OpenaiClient;
+import me.vacuity.ai.sdk.openai.error.ChatResponseError;
+import me.vacuity.ai.sdk.openai.exception.VacSdkException;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
